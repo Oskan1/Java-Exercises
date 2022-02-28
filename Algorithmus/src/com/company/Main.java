a969d9a169cebf80c9f2b22baa86bced6e089581
Oskan1 <100508408+Oskan1@users.noreply.github.com>
2022-02-28 01:15:43 +0100

    Update Main.java

--- Algorithmus/src/com/company/Main.java
+++ Algorithmus/src/com/company/Main.java
@@ -17,14 +17,5 @@ public class Main {
         System.out.println(n - 1);
 
     }
-    public static void newAlgo(int x, int b){
-        int n = 0;
-        int p = 1;
-        while (p <= x) {
-            n += 1;
-            p *= b;
-        }
-        System.out.println(n - 1);
-    }
 
 }
